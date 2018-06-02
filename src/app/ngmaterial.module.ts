132bcf095858ec105548dde6962f8bc0e6c79113
Richard Wolf <rich@pieinsurance.com>
2018-06-02 15:51:32 -0600

    Angular what

---
+++ src/app/ngmaterial.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {  MatButtonModule } from '@angular/material';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+
+@NgModule({
+imports: [MatButtonModule, MatToolbarModule],
+exports: [MatButtonModule, MatToolbarModule]
+})
+
+export class MaterialAppModule { }
